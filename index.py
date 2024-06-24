2abc670a2ef056dfafbe16d880404d9bf28812ac
xydov <noahchelgham@gmail.com>
2024-06-24 20:20:27 +0200

    update retrieve

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import pyterrier as pt
 import ir_datasets
 import pandas as pd
 import os
+import wn
 
 def document_generator(dataset):
     for doc in dataset.docs_iter():
@@ -29,6 +30,41 @@ def preprocess_query(query):
     query = query.strip()
     return query
 
+def get_synonyms(word):
+    synonyms = set()
+    for syn in wn.synsets(word):
+        for lemma in syn.lemmas():
+            synonyms.add(lemma.name().replace('_', ' '))
+    return synonyms
+
+def expand_query(query):
+    words = query.split()
+    expanded_queries = set()
+    expanded_queries.add(query)
+
+    for word in words:
+        synonyms = get_synonyms(word)
+        for synonym in synonyms:
+            new_query = query.replace(word, synonym)
+            expanded_queries.add(new_query)
+
+    return list(expanded_queries)
+
+def treat_querie(text):
+
+  expanded_queries = expand_query(text)
+  final_req=[]
+  for eq in expanded_queries:
+      final_req.append(eq)
+  tokens = set()
+  for sentence in final_req:
+      words = word_tokenize(sentence)
+      tokens.update(words)  # Add tokens to the set
+      sentence = ' '.join(tokens)
+      return sentence
+
+
+
 def save_queries(dataset, queries_path='./queries.csv'):
     queries_list = []
     for query in dataset.queries_iter():
@@ -41,6 +77,7 @@ def save_queries(dataset, queries_path='./queries.csv'):
 if __name__ == "__main__":
     dataset = ir_datasets.load("vaswani")
     index_ref, index_path = create_index(dataset)
+    queries = [query.text for query in dataset.queries_iter()]
     
     # Save the index reference path to a file
     index_ref_path = './index_ref.txt'
